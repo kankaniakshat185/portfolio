7812e3ef16166b8102ba7f7eed1b35f34e59f526
Akshat Kankani <157486069+kankaniakshat185@users.noreply.github.com>
2026-01-30 11:31:55 +0530

    updates

--- app/footer.tsx
+++ app/footer.tsx
@@ -73,7 +73,7 @@ export function Footer() {
         <a href="https://github.com/kankaniakshat185" target="_blank">
           <TextLoop className="text-xs text-zinc-500">
             <span>Akshat Kankani</span>
-            <span>Always thinking, slow and steady.</span>
+            <span>Always learning, Always building.</span>
           </TextLoop>
         </a>
         <div className="text-xs text-zinc-400">
