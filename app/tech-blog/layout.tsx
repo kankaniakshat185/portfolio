0d2ced761f21560ee8658452185ab33172c7d89e
Akshat Kankani <157486069+kankaniakshat185@users.noreply.github.com>
2026-08-04 01:47:09 +0530

    update link button position in tech blogs

--- app/tech-blog/layout.tsx
+++ app/tech-blog/layout.tsx
@@ -49,31 +49,33 @@ export default function LayoutBlogPost({
         }}
       />
 
-      <div className="absolute right-4 top-24 flex items-center gap-2">
-        {currentPost?.githubLink && (
-          <a
-            href={currentPost.githubLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-flex h-8 w-8 items-center justify-center rounded-full border border-zinc-200 bg-white text-zinc-900 transition-colors hover:bg-zinc-100 dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-400 dark:hover:bg-zinc-800"
-            aria-label="GitHub Repository"
-          >
-            <Github className="h-4 w-4" />
-          </a>
-        )}
-        {currentPost?.liveLink && (
-          <a
-            href={currentPost.liveLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-flex h-8 w-8 items-center justify-center rounded-full border border-zinc-200 bg-white text-zinc-900 transition-colors hover:bg-zinc-100 dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-400 dark:hover:bg-zinc-800"
-            aria-label="Live Deployment"
-          >
-            <LinkIcon className="h-4 w-4" />
-          </a>
-        )}
-      </div>
       <main className="prose prose-gray max-w-none mt-24 pb-20 prose-h4:prose-base dark:prose-invert prose-h1:text-xl prose-h1:font-medium prose-h2:mt-12 prose-h2:scroll-m-20 prose-h2:text-lg prose-h2:font-medium prose-h3:text-base prose-h3:font-medium prose-h4:font-medium prose-h5:text-base prose-h5:font-medium prose-h6:text-base prose-h6:font-medium prose-strong:font-medium">
+        {(currentPost?.githubLink || currentPost?.liveLink) && (
+          <div className="float-right flex items-center gap-2 ml-4 mb-2 mt-0.5">
+            {currentPost?.githubLink && (
+              <a
+                href={currentPost.githubLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-flex h-8 w-8 items-center justify-center rounded-full border border-zinc-200 bg-white text-zinc-900 transition-colors hover:bg-zinc-100 dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-400 dark:hover:bg-zinc-800"
+                aria-label="GitHub Repository"
+              >
+                <Github className="h-4 w-4" />
+              </a>
+            )}
+            {currentPost?.liveLink && (
+              <a
+                href={currentPost.liveLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-flex h-8 w-8 items-center justify-center rounded-full border border-zinc-200 bg-white text-zinc-900 transition-colors hover:bg-zinc-100 dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-400 dark:hover:bg-zinc-800"
+                aria-label="Live Deployment"
+              >
+                <LinkIcon className="h-4 w-4" />
+              </a>
+            )}
+          </div>
+        )}
         {children}
       </main>
     </>
